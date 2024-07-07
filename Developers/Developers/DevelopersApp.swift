6ffb1361d74182e9afa6c883dd374c96ca9973a2
Saamu02 <iusamairfan@gmail.com>
2024-07-07 21:14:23 +0500

    Project created

---
+++ Developers/Developers/DevelopersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopersApp.swift
+//  Developers
+//
+//  Created by Ussama Irfan on 07/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct DevelopersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
