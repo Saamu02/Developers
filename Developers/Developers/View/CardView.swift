505e20b8d7ccc576fe1da844f59433adbd41e871
Saamu02 <iusamairfan@gmail.com>
2024-07-08 00:22:06 +0500

    Assets added

---
+++ Developers/Developers/View/CardView.swift
@@ -0,0 +1,19 @@
+//
+//  CardView.swift
+//  Developers
+//
+//  Created by Ussama Irfan on 08/07/2024.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
